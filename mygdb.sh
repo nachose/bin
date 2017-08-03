6ab6db46e151941c731f177c2169909d6d80f0c7
Jose Ignacio Seco <joseignacio.seco@gmail.com>
2017-08-03 10:47:13 +0200

    add more files

---
+++ mygdb.sh
@@ -0,0 +1,1 @@
+/opt/bcm/host/bin/mipsel-linux-gdb --command=gdb_commands.txt
---
+++ mysql_cont.sh
@@ -0,0 +1,2 @@
+docker run -d -p 3306:3306 -e MYSQL_ROOT_PASSWORD=root_pass -e MYSQL_DATABASE=stats_database -e MYSQL_USER=nacho -e MYSQL_PASSWORD=nacho_pass centurylink/mysql
+hg log . --style=xml
